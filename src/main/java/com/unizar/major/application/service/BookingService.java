103e76744d935a871fc7b1ddea3be749ead1e612
osboxes <osboxes.org>
2019-05-16 13:06:16 -0400

    Booking cumple politica

--- src/main/java/com/unizar/major/application/service/BookingService.java
+++ src/main/java/com/unizar/major/application/service/BookingService.java
@@ -57,30 +57,40 @@ public class BookingService {
     public Boolean createNewBooking(Long id, BookingDto bookingDto) {
         Optional<User> user = userRepository.findById(id);
         if (user.isPresent()) {
-            User u = user.get();
+            if (cumplePolitica(bookingDto,user.get().getRol())){
+                User u = user.get();
 
-            Period period = new Period(bookingDto.getPeriod().getstartDate(), bookingDto.getPeriod().getEndDate());
-            List<Period> p = new ArrayList<>();
-            p.add(period);
+                Period period = new Period(bookingDto.getPeriod().getstartDate(), bookingDto.getPeriod().getEndDate());
+                List<Period> p = new ArrayList<>();
+                p.add(period);
 
-            Booking booking = new Booking(bookingDto.isIsPeriodic(), bookingDto.getReason(), p);
-            booking.setActive(true);
-            booking.setState("inicial");
-            booking.setFinalDate(null);
-            booking.setPeriodRep(null);
-            booking.setUser(u);
+                Booking booking = new Booking(bookingDto.isIsPeriodic(), bookingDto.getReason(), p);
+                booking.setActive(true);
+                booking.setState("inicial");
+                booking.setFinalDate(null);
+                booking.setPeriodRep(null);
+                booking.setUser(u);
 
-            for (int i = 0; i < bookingDto.getSpaces().size(); i++) {
-                Optional<Space> space = spaceRepository.findByGid(bookingDto.getSpaces().get(i));
-                booking.setSpaces(space.get());
+                for (int i = 0; i < bookingDto.getSpaces().size(); i++) {
+                    Optional<Space> space = spaceRepository.findByGid(bookingDto.getSpaces().get(i));
+                    booking.setSpaces(space.get());
+                }
+
+                booking.setEspecial(bookingDto.isEspecial());
+
+                bookingRepository.save(booking);
+                return true;
+            }
+            else{
+                return false;
             }
 
-            booking.setEspecial(bookingDto.isEspecial());
 
-            bookingRepository.save(booking);
-            return true;
         }
-        return false; //404
+        else{
+            return false; //404
+        }
+
     }
 
     @Transactional
@@ -88,33 +98,119 @@ public class BookingService {
         Optional<User> user = userRepository.findById(id);
         List<Space> spaces = new ArrayList<>();
         if (user.isPresent()) {
-            User u = user.get();
-            List<Period> p = calculatePeriods(bookingDto.getPeriodRep(), bookingDto.getFinalDate(), bookingDto.getPeriod().getstartDate(), bookingDto.getPeriod().getEndDate());
-            Booking booking = new Booking(bookingDto.isIsPeriodic(), bookingDto.getReason(), p, bookingDto.getPeriodRep(), bookingDto.getFinalDate());
-            booking.setActive(true);
-            booking.setState("inicial");
-            booking.setUser(u);
+            if (cumplePolitica(bookingDto,user.get().getRol())) {
+                User u = user.get();
+                List<Period> p = calculatePeriods(bookingDto.getPeriodRep(), bookingDto.getFinalDate(), bookingDto.getPeriod().getstartDate(), bookingDto.getPeriod().getEndDate());
+                Booking booking = new Booking(bookingDto.isIsPeriodic(), bookingDto.getReason(), p, bookingDto.getPeriodRep(), bookingDto.getFinalDate());
+                booking.setActive(true);
+                booking.setState("inicial");
+                booking.setUser(u);
 
-            for (int i = 0; i < bookingDto.getSpaces().size(); i++) {
-                Optional<Space> space = spaceRepository.findByGid(bookingDto.getSpaces().get(i));
-                booking.setSpaces(space.get());
+                for (int i = 0; i < bookingDto.getSpaces().size(); i++) {
+                    Optional<Space> space = spaceRepository.findByGid(bookingDto.getSpaces().get(i));
+                    booking.setSpaces(space.get());
+                }
+
+                if (u.getRol() == User.Rol.ADMIN) {
+                    booking.setEspecial(bookingDto.isEspecial());
+                } else {
+                    booking.setEspecial(false);
+                }
+
+                bookingRepository.save(booking);
+                return true;
             }
-
-            if (u.getRol() == User.Rol.ADMIN) {
-                booking.setEspecial(bookingDto.isEspecial());
-            } else {
-                booking.setEspecial(false);
+            else{
+                return false;
             }
-
-            bookingRepository.save(booking);
-            return true;
         }
         return false;
     }
 
-    public boolean cumplePolitica(BookingDto bookingDto) {
+    public boolean cumplePolitica(BookingDto bookingDto, User.Rol user_rol) {
 
-        return true;
+        boolean cumplePolitica = true;
+
+        Calendar fecha_ini= Calendar.getInstance();
+        fecha_ini.setTime(bookingDto.getPeriod().getstartDate());
+
+        Calendar fecha_fin = Calendar.getInstance();
+        fecha_fin.setTime(bookingDto.getPeriod().getEndDate());
+
+        Calendar calendar_mes = Calendar.getInstance();
+        calendar_mes.add(Calendar.MONTH, 1);
+
+        Calendar calendar_semana = Calendar.getInstance();
+        calendar_semana.add(Calendar.DATE, 7);
+
+        Calendar hoy = Calendar.getInstance();
+
+        if (fecha_ini.after(hoy)){
+
+            if (user_rol==User.Rol.PAS) {
+                if (bookingDto.isIsPeriodic()){
+                    if (fecha_ini.after(calendar_mes)) {
+                        cumplePolitica = false;
+                    }
+                }
+                else{
+                    if (fecha_ini.after(calendar_mes) || fecha_fin.after(calendar_mes)) {
+                        cumplePolitica = false;
+                    }
+                }
+            }
+            else if (user_rol==User.Rol.ESTUDIANTE){
+                if(fecha_ini.after(calendar_semana) || fecha_fin.after(calendar_semana)){
+                    cumplePolitica =false;
+                }
+            }
+
+            if (cumplePolitica) {
+
+                for (int i = 0; i < bookingDto.getSpaces().size(); i++) {
+                    Optional<Space> space = spaceRepository.findByGid(bookingDto.getSpaces().get(i));
+                    List<Booking> bookings = space.get().getBookings();
+
+                    if (!bookings.isEmpty()) {
+
+                        for (int j = 0; j < bookings.size(); j++) {
+                            Booking booking = bookings.get(j);
+                            if (booking.isActive()) {
+                                Collection<Period> p = booking.getPeriod();
+                                for (int k = 0; k < p.size(); k++) {
+                                    Calendar ini = Calendar.getInstance();
+                                    ini.setTime(booking.getPeriod().get(k).getstartDate());
+                                    Calendar fin = Calendar.getInstance();
+                                    fin.setTime(booking.getPeriod().get(k).getEndDate());
+
+                                    if (ini.equals(fecha_ini)) {
+                                        cumplePolitica = false;
+                                    } else if (fecha_ini.after(ini) && fecha_fin.before(fin)) {
+                                        cumplePolitica = false;
+                                    } else if (fecha_fin.after(ini) && fecha_fin.before(fin)) {
+                                        cumplePolitica = false;
+                                    } else if (fecha_ini.after(ini) && fecha_ini.before(fin)) {
+                                        cumplePolitica = false;
+                                    } else if (fin.equals(fecha_fin)) {
+                                        cumplePolitica = false;
+                                    } else if (fecha_ini.before(ini) && fecha_fin.after(fin)) {
+                                        cumplePolitica = false;
+                                    }
+
+                                }
+                            }
+                        }
+                    } else {
+                        cumplePolitica = true;
+                    }
+                }
+            }
+
+        } else{
+            cumplePolitica =false;
+        }
+
+        return cumplePolitica;
 
     }
 
@@ -139,16 +235,20 @@ public class BookingService {
 
                 while (calendar.getTime().compareTo(finalDate) < 0) {
 
-                    Period period2 = new Period();
-                    period2.setstartDate(calendar.getTime());
-                    startDate = calendar.getTime();
-                    calendar.setTime(endDate);
-                    calendar.add(Calendar.DATE, 1);
-                    period2.setEndDate(calendar.getTime());
-                    endDate = calendar.getTime();
-                    p.add(period2);
-                    calendar.setTime(startDate);
-                    calendar.add(Calendar.DATE, 1);
+                        Period period2 = new Period();
+                        period2.setstartDate(calendar.getTime());
+                        startDate = calendar.getTime();
+                        calendar.setTime(endDate);
+                        calendar.add(Calendar.DATE, 1);
+                        period2.setEndDate(calendar.getTime());
+                        endDate = calendar.getTime();
+                        int dia = calendar.get(Calendar.DAY_OF_WEEK);
+                        if (dia!=1 && dia!=7){
+                            p.add(period2);
+                        }
+                        calendar.setTime(startDate);
+                        calendar.add(Calendar.DATE, 1);
+
                 }
 
                 break;
