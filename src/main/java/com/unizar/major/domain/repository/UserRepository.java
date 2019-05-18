92cf16ce695ae362cfa2ed3bfdc3f8b15b94e83a
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2019-05-18 17:56:38 +0200

    UserService only fetch active users
    Signed-off-by: Jorge Rambla Gonzalez <yorch044zgz@gmail.com>

--- src/main/java/com/unizar/major/domain/repository/UserRepository.java
+++ src/main/java/com/unizar/major/domain/repository/UserRepository.java
@@ -2,20 +2,26 @@ package com.unizar.major.domain.repository;
 
 import com.unizar.major.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
+    @Query(value = "SELECT * FROM personeina WHERE active", nativeQuery = true)
     List<User> findAll();
-    Optional<User> findById(Long id);
-    Optional<User> findByUserName(String nameUser);
-    Optional<User> findByEmail(String email);
+
+    @Query(value = "SELECT * FROM personeina WHERE iduser = :id AND active", nativeQuery = true)
+    Optional<User> findById(@Param("id") Long id);
+
+    @Query(value = "SELECT * FROM personeina WHERE nombreusuario = :nameUser AND active", nativeQuery = true)
+    Optional<User> findByUserName(@Param("nameUser") String nameUser);
+
+    @Query(value = "SELECT * FROM personeina WHERE email = :email AND active", nativeQuery = true)
+    Optional<User> findByEmail(@Param("email") String email);
 
 }
